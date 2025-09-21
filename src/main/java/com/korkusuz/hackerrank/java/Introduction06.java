a1eb4f80d73db0ed190dc069bd4740dd0e2f6594
kadirkorkusuz <kadirkorkusuz22@gmail.com>
2025-09-21 16:06:19 +0200

    feat: Hackerrank - Java - Introduction - Solution 6

---
+++ src/main/java/com/korkusuz/hackerrank/java/Introduction06.java
@@ -0,0 +1,20 @@
+package com.korkusuz.hackerrank.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Introduction06 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(N + " x " + i + " = " + (N * i));
+        }
+
+        bufferedReader.close();
+    }
+}
