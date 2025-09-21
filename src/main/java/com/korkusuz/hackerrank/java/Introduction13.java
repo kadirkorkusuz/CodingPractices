36cabc146895ba3a33ccae878f7dfde8736c920a
kadirkorkusuz <kadirkorkusuz22@gmail.com>
2025-09-21 16:34:44 +0200

    feat: Hackerrank - Java - Introduction - Solution 13

---
+++ src/main/java/com/korkusuz/hackerrank/java/Introduction13.java
@@ -0,0 +1,30 @@
+package com.korkusuz.hackerrank.java;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Introduction13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        NumberFormat usFormat = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat indiaFormat = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+        NumberFormat chinaFormat = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat franceFormat = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+        String us = usFormat.format(payment);
+        String india = indiaFormat.format(payment).replace("\u20B9", "Rs.");
+        String china = chinaFormat.format(payment);
+        String france = franceFormat.format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
